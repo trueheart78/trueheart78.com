c4090d5af58d05fe5239bddc7fe0d94b7d37f17d
Josh Mills <josh@trueheart78.com>
2021-12-26 23:13:02 -0500

    log stuff from games.js

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -9,6 +9,7 @@
 const dates = [];
 
 async function loadGameData() {
+  console.log("loadGameData: start");
   let response = await fetch('https://api.trueheart78.com/v1/games/games.json');
 
   if (!response.ok) {
@@ -16,12 +17,16 @@ async function loadGameData() {
   }
 
   let gameData = await response.json();
+  console.log("loadGameData: json ready");
   dates.push(gameData.last_modified);
+  console.log("loadGameData: date pushed? ["+gameData.last_modified+"]");
 
   parseGames(gameData.games, gameData.statuses);
+  console.log("loadGameData: end");
 }
 
 async function loadPurchaseData() {
+  console.log("loadPurchaseData: start");
   let response = await fetch('https://api.trueheart78.com/v1/games/purchases.json');
 
   if (!response.ok) {
@@ -29,12 +34,16 @@ async function loadPurchaseData() {
   }
 
   let purchaseData = await response.json();
+  console.log("loadPurchaseData: json loaded");
   dates.push(purchaseData.last_modified);
+  console.log("loadPurchaseData: date pushed? ["+purchaseData.last_modified+"]");
 
   parsePurchases(purchaseData.purchases, purchaseData.categories, purchaseData.completed_statuses);
+  console.log("loadPurchaseData: end");
 }
 
 async function loadLessonData() {
+  console.log("loadLessonData: start");
   let response = await fetch('https://api.trueheart78.com/v1/games/lessons.json');
 
   if (!response.ok) {
@@ -42,9 +51,12 @@ async function loadLessonData() {
   }
 
   let lessonData = await response.json();
+  console.log("loadLessonData: start");
   dates.push(lessonData.last_modified);
+  console.log("loadLessonData: date pushed? ["+lessonData.last_modified+"]");
 
   parseLessons(lessonData.lessons);
+  console.log("loadLessonData: end");
 }
 
 function loadData() {
@@ -56,7 +68,11 @@ function loadData() {
 }
 
 function updateLastModified(dates) {
-  document.getElementById("last-modified").innerHTML = longDate(dates[0]);
+  if (dates.length > 0) {
+    let mostRecentDate = dates.sort().pop();
+    
+    document.getElementById("last-modified").innerHTML = longDate(mostRecentDate);
+  }
 }
 
 function longDate(date) {
