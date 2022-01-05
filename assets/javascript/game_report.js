14014d4cc56186ac5b626218a28603612f884e30
Josh Mills <josh@trueheart78.com>
2022-01-04 22:57:29 -0500

    Updated game_report.js to detectReportDate() using params.date

--- assets/javascript/game_report.js
+++ assets/javascript/game_report.js
@@ -1,8 +1,10 @@
 const urlSearchParams = new URLSearchParams(window.location.search);
 const params = Object.fromEntries(urlSearchParams.entries());
-const today = new Date();
+
+const today = detectReportDate();
 const currentYear = today.getFullYear();
 const currentMonth = today.getMonth();
+
 const gamePassHeartURL = "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/facebook/65/green-heart_1f49a.png";
 
 async function loadGameData() {
@@ -86,15 +88,31 @@ function updateGamePassHearts() {
   }
 }
 
+function isThisMonth(date) {
+  let d = new Date(date);
+  let year = d.getFullYear();
+  let month = d.getMonth();
+
+  return ((year == currentYear) && (month == currentMonth));
+}
+
+function detectReportDate() {
+  if (params.hasOwnProperty("date") {
+    return new Date(params.date);
+  }
+
+  return new Date();
+}
+
 function copyReport() {
   var range = document.createRange();
-  range.selectNode(document.getElementById('bbcode-report'));
+  range.selectNode(document.getElementById("bbcode-report"));
   window.getSelection().removeAllRanges();
   window.getSelection().addRange(range);
-  document.execCommand('copy');
+  document.execCommand("copy");
   window.getSelection().removeAllRanges();
 
-  document.getElementById('copy-report-button').value = 'Copied!';
+  document.getElementById("copy-report-button").value = "Copied!";
 }
 
 function logError(error) {
